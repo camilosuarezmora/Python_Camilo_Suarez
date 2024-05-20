4dd2069b5220e74b144ba41b3600b3bde6c3aac3
camilosuarezmora <camilosuarezm2007@gmail.com>
2024-05-20 13:03:18 -0500

    Inicial

---
+++ Python/Ejemplo ciclo for.py
@@ -0,0 +1,16 @@
+listaa = [1, 2, 3] #Elemento iterable
+for variableAlmacenaIteraciones in listaa:
+    print(variableAlmacenaIteraciones)
+    
+#OUTPUT
+  #1
+  #2
+  #3
+  
+for variableAlmacenaIteraciones in range(1, 5, 1):
+    print(variableAlmacenaIteraciones, end = ", ")
+  
+#OUTPUT
+ #1, 2, 3, 4
+ 
+ 
